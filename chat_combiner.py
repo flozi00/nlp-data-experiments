c0b12e4c1e0b6994eb583fae31570d2a8d25048b
flozi00 <flozi00.fz@gmail.com>
2023-07-06 15:48:46 +0200

    naming

--- chat_combiner.py
+++ chat_combiner.py
@@ -8,6 +8,7 @@ END = "<|endoftext|>"
 def get_chat_dataset() -> datasets.Dataset:
     all_rows = []
 
+    print("Argilla 15K")
     ds = datasets.load_dataset(
         "argilla/databricks-dolly-15k-curated-multilingual", split="de+en"
     )
@@ -17,14 +18,28 @@ def get_chat_dataset() -> datasets.Dataset:
                 f'{PROMPTER}{row["context"]}\n{row["instruction"]}{END}{BOT}{row["response"]}{END}'  # type: ignore
             )
 
+    print("Guanaco German 17K")
+    ds = datasets.load_dataset("sixf0ur/GuanacoDataset-de", split="train")
+    for row in ds:
+        all_rows.append(
+            f'{PROMPTER}{row["input"].replace("User:", "")}{END}{BOT}{row["output"]}{END}'  # type: ignore
+        )
+        all_rows.append(
+            row["instruction"]
+            .replace(" User:", END + PROMPTER)
+            .replace("Assistent:", END + BOT)
+            .replace("User:", PROMPTER)
+        )
+
+    print("OASST filtered")
     ds = datasets.load_dataset(
         "flozi00/openassistant-oasst1-flattened-filtered", split="train"
     ).filter(lambda example: example["lang"] in ["de", "en"])
-
     for x in ds:
         all_rows.append(x["conversations"])
 
-    ds = datasets.load_dataset("flozi00/german-conversations", split="train")
+    print("german conversations")
+    ds = datasets.load_dataset("flozi00/oasst1-en-to-de", split="train")
     for x in ds:
         all_rows.append(x["conversations"])
 
