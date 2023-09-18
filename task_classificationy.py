c1a340e03e2bbebefed3124257373da22e1369f1
flozi00 <flozi00.fz@gmail.com>
2023-09-18 11:39:30 +0000

    fix

--- task_classificationy.py
+++ task_classificationy.py
@@ -6,10 +6,13 @@ modes = []
 all_rows = []
 
 ds = datasets.load_dataset(
-    "argilla/databricks-dolly-15k-curated-multilingual", split="de+en"
+    "argilla/databricks-dolly-15k-curated-multilingual", split="de+en+es+fr"
 )
+labels = ds.unique("category")
 for row in tqdm(ds, desc="Databricks Dolly"):
-    all_rows.append(f'{row["context"]}\n{row["instruction"]}')
+    all_rows.append(
+        f'Labels: {", ".join(labels)} </s> Input: {row["context"]}\n{row["instruction"]}'
+    )
     modes.append(row["category"])
 
 ds = datasets.Dataset.from_dict({"text": all_rows, "label": modes})
